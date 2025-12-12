b80a7e4ac54919df0dca17742f3e1132dcc17151
Lazarev Vlad <66120838+victornew@users.noreply.github.com>
2025-12-12 07:29:59 +0200

    3

--- app/SEEME_TASK_Calculate.cpp
+++ app/SEEME_TASK_Calculate.cpp
@@ -11,10 +11,20 @@ double areaSquare(double a) {
     return a * a;
 }
 
+double areaRectangle(double a, double b) {
+    return a * b;
+}
+
+double areaTriangle(double a, double h) {
+    return 0.5 * a * h;
+}
+
 int main() {
     cout << "Calculator of areas" << endl;
     cout << "1 - circle" << endl;
     cout << "2 - square" << endl;
+    cout << "3 - rectangle" << endl;
+    cout << "4 - triangle" << endl;
     cout << "Choose figure: ";
 
     int choice;
@@ -25,12 +35,30 @@ int main() {
         cout << "Enter radius: ";
         cin >> r;
         cout << "Area = " << areaCircle(r) << endl;
-    } else if (choice == 2) {
+    }
+    else if (choice == 2) {
         double a;
         cout << "Enter side: ";
         cin >> a;
         cout << "Area = " << areaSquare(a) << endl;
-    } else {
+    }
+    else if (choice == 3) {
+        double a, b;
+        cout << "Enter side a: ";
+        cin >> a;
+        cout << "Enter side b: ";
+        cin >> b;
+        cout << "Area = " << areaRectangle(a, b) << endl;
+    }
+    else if (choice == 4) {
+        double a, h;
+        cout << "Enter base: ";
+        cin >> a;
+        cout << "Enter height: ";
+        cin >> h;
+        cout << "Area = " << areaTriangle(a, h) << endl;
+    }
+    else {
         cout << "Not implemented yet" << endl;
     }
 
