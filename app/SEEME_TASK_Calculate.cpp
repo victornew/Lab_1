bd8b1bf6aff1295eb50a6bc3dd44af909bff62e8
Lazarev Vlad <66120838+victornew@users.noreply.github.com>
2025-12-12 07:26:59 +0200

    2

--- app/SEEME_TASK_Calculate.cpp
+++ app/SEEME_TASK_Calculate.cpp
@@ -7,9 +7,14 @@ double areaCircle(double r) {
     return PI * r * r;
 }
 
+double areaSquare(double a) {
+    return a * a;
+}
+
 int main() {
     cout << "Calculator of areas" << endl;
     cout << "1 - circle" << endl;
+    cout << "2 - square" << endl;
     cout << "Choose figure: ";
 
     int choice;
@@ -20,6 +25,11 @@ int main() {
         cout << "Enter radius: ";
         cin >> r;
         cout << "Area = " << areaCircle(r) << endl;
+    } else if (choice == 2) {
+        double a;
+        cout << "Enter side: ";
+        cin >> a;
+        cout << "Area = " << areaSquare(a) << endl;
     } else {
         cout << "Not implemented yet" << endl;
     }
